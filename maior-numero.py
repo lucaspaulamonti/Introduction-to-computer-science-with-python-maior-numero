60d133c6a86330175a018a06ec33fa3f28f5150d
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 13:52:43 -0300

    Create maior-numero.py

---
+++ maior-numero.py
@@ -0,0 +1,9 @@
+# Escreva a função maximo que recebe 2 números inteiros como parâmetro e devolve o maior deles.
+def maximo(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+maximo(4,5)
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
